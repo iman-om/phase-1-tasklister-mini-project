f51d5571e6126fb080a44825b6ee7ddbd49cc24b
imane omari <imane3.omari@gmail.com>
2023-08-11 22:23:01 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      const newTask = document.createElement("li");
+      newTask.innerText = taskText;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "Delete";
+      deleteButton.addEventListener("click", () => {
+        taskList.removeChild(newTask);
+      });
+
+      newTask.appendChild(deleteButton);
+      taskList.appendChild(newTask);
+
+      taskInput.value = "";
+    }
+  });
 });
